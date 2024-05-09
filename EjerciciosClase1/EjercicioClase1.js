e21fc852a928aefb2824a5f132cbab008b5acf3f
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-05-09 10:25:24 -0600

    Ejercicios completos

--- EjerciciosClase1/EjercicioClase1.js
+++ EjerciciosClase1/EjercicioClase1.js
@@ -5,20 +5,24 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+//Ejercicio 1
 rl.question('Ingrese su nombre: ', (nombre) => {
    rl.question('Ingrese su apellido: ',(apellido) =>{
        console.log(`Hola mi nombre es ${nombre} ${apellido}`)
    })
 })
 
+//Ejercicio 2
 rl.question('Ingrese una cadena de texto: ', (cadena) => {
    console.log(`Numero de caracteres: ${cadena.length}`)    
 })
 
+//Ejercicio 3
 rl.question('Ingrese una palabra: ', (palabra) => {
    console.log(palabra.toUpperCase())    
 })
 
+//Ejercicio 4
 rl.question('Ingrese una frase: ', (frase) => {
     rl.question('Ingrese la palabra que quiera remplazar: ', (palabraCambiar) => {
         rl.question('Ingrese la palabra nueva: ', (palabraNueva) => {
@@ -28,6 +32,7 @@ rl.question('Ingrese una frase: ', (frase) => {
     })    
 })
 
+//Ejercicio 5
 rl.question('Ingrese una cadena: ', (cadena) => {
     const inicio  = 6;   
     const longitud = 5;
@@ -35,16 +40,19 @@ rl.question('Ingrese una cadena: ', (cadena) => {
     console.log(`Subcadena es: ${subcadena}`)
  })
 
+ //Ejercicio 6
 rl.question('Ingrese una frase: ', (frase) => {
    const palabras = frase.split(' ');
    console.log(`Numero de palabras: ${palabras.length}`)    
 })
 
+//Ejercicio 7
 rl.question('Ingrese una cadena: ', (cadena) => {
    const cadenaInvertida = cadena.split('').reverse().join('');
    console.log(`Cadena invertida: ${cadenaInvertida}`)    
 })
 
+//Ejercicio 8
 rl.question('Ingrese una palabra: ', (palabra) => {
     const palabraInvertida = palabra.split('').reverse().join('');
     if(palabra === palabraInvertida){
@@ -54,12 +62,14 @@ rl.question('Ingrese una palabra: ', (palabra) => {
     }    
  })
 
+ //Ejercicio 9
 rl.question('Ingrese una palabra: ', (palabra) => {
     const vocales = palabra.match(/[aeiou]/gi);
     const numeroVocales = vocales ? vocales.length:0;
     console.log(`Numero de vocales: ${numeroVocales}`)
  })
 
+ //Ejercicio 10
 rl.question('Ingrese una frase: ', (frase) => {
     const palabras = frase.split(' ');
     const fraseCapitalizada = palabras.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('')
