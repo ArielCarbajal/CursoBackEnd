d31aaf2635381348ba2e231abe1efb4de4f5b2ea
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-05-14 16:50:05 -0600

    Instalacion nodemail + practica enviar correo completa

--- Modulo1/Clase5/Clase5.js
+++ Modulo1/Clase5/Clase5.js
@@ -6,16 +6,40 @@
 // console.log(arrayAcomodado)
 
 //Ejercicio 2
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-const PORT = 4000;
+// const PORT = 4000;
 
-app.get('/',(req, res)=>{
-    res.send("Servidor basico usando express")
-})
+// app.get('/',(req, res)=>{
+//     res.send("Servidor basico usando express")
+// })
 
-app.listen(PORT, ()=>{
-    console.log(`Server iniciado http://localhost:${PORT}`)
-})
+// app.listen(PORT, ()=>{
+//     console.log(`Server iniciado http://localhost:${PORT}`)
+// })
 
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+        user: 'ariel.carbajal21@itca.edu.sv',
+        pass: '' //Sin contraseña pa que no me roben :D
+    }
+});
+
+let mailOptions = {
+    from: 'ariel.carbajal21@itca.edu.sv',
+    to: 'j.william03@hotmail.com', 
+    subject: 'Practica Correo', 
+    text: 'Servidor de correo funcionando, Ariel Enoc Carbajal Amaya' 
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Correo electrónico enviado: ' + info.response);
+    }
+});
