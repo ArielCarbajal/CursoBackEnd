b873ec5830905f9002902fa8c297106b2bb6f320
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-05-14 16:33:36 -0600

    Instalacion de express y nodemon

--- Modulo1/Clase5/Clase5.js
+++ Modulo1/Clase5/Clase5.js
@@ -1,5 +1,21 @@
-const shuffle = require('lodash/shuffle')
+//Ejercicio 1
+// const shuffle = require('lodash/shuffle')
 
-const array = [9,3,5,3,4]
-const arrayAcomodado = shuffle(array)
-console.log(arrayAcomodado)
+// const array = [9,3,5,3,4]
+// const arrayAcomodado = shuffle(array)
+// console.log(arrayAcomodado)
+
+//Ejercicio 2
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+app.get('/',(req, res)=>{
+    res.send("Servidor basico usando express")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server iniciado http://localhost:${PORT}`)
+})
+
