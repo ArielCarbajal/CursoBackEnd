dc76d01d71b5e350e91b26f9ba853681f649daba
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-05-21 15:07:38 -0600

    Clase4

---
+++ Modulo2/Clase4/index.js
@@ -0,0 +1,23 @@
+// const {agregar, restar} = require('./funciones')
+
+// const resultado = agregar(5, 3)
+// const residuo = restar(10, 2)
+// console.log(`El resultado de la suma es: ${resultado}`)
+// console.log(`El resultado de la resta es: ${residuo}`)
+
+// const persona = require('./funciones')
+// const persona1 = new persona('Antonio', 35) 
+// persona1.saludo()
+
+// const fs = require('fs')
+// const formatoFecha = require('./funciones')
+
+// const hoy
+
+import { suma , resta } from './funciones.js'
+
+const resultadoSuma = suma(10,2)
+const resultadoResta = resta(8,1)
+
+console.log(`El resultado de la suma es: ${resultadoSuma}`);
+console.log(`El resultado de la resta es: ${resultadoResta}`);
